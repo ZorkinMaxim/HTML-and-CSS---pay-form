67beaaef94b55a3a2dab14b7ec56591eadbc9d76
ZorkinMaxim <74238953+ZorkinMaxim@users.noreply.github.com>
2021-04-12 15:04:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function myFunction() {
+//     document.getElementById("id_pay").style.color = 'red';
+// }
+
+document.querySelector('#push').onclick = () => {
+    let n = document.querySelector('#fname').value;
+    let s = document.querySelector('#lname').value;
+    console.log(n + " " + s)
+}
+
